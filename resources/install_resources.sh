39862eecf6eed596331b46efe1a24a833867f1d5
Juan Lozano <libredeb@gmail.com>
2020-08-15 16:46:20 -0300

    Add common packages and lightpad installation

--- resources/install_resources.sh
+++ resources/install_resources.sh
@@ -11,7 +11,18 @@ fi
 
 install_debian_packages() {
     echo -e "Installing required packages..."
-    apt-get install gtk2-engines-murrine gtk2-engines-pixbuf gnome-icon-theme
+    apt-get update
+    apt-get install gtk2-engines-murrine gtk2-engines-pixbuf gnome-icon-theme plank catfish appmenu-gtk2-module appmenu-gtk3-module appmenu-qt xfce4-appmenu-plugin xfce4-notifyd meson ninja-build libgee-0.8-dev libgnome-menu-3-dev cdbs valac git libvala-0.42-dev libglib2.0-dev libwnck-3-dev libgtk-3-dev xterm python3 python3-wheel python3-setuptools gnome-menus
+}
+
+install_and_compile_lightpad() {
+    git clone https://github.com/libredeb/lightpad.git /tmp/lightpad
+    cd /tmp/lightpad
+    meson build --prefix=/usr
+    cd build
+    ninja
+    sudo ninja install
+    rm -Rf /tmp/lightpad
 }
 
 install_gtk_themes() {
@@ -37,6 +48,7 @@ install_fonts() {
 }
 
 install_debian_packages
+install_and_compile_lightpad
 install_gtk_themes
 install_icon_themes
 install_fonts
